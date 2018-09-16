dcc0762a7fb41d488a4c0887db90369ecf998168
lifedok <lifedok@gmail.com>
2018-09-17 00:03:53 +0300

    Обработка событий

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Wizard = {
+  CONST: 4,
   FIRST_NAME: [
     'Иван',
     'Хуан Себастьян',
@@ -35,6 +36,13 @@ var Wizard = {
     'blue',
     'yellow',
     'green'
+  ],
+  FIREBALL: [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
   ]
 };
 
@@ -46,7 +54,7 @@ var SETUP_SIMILAR = document.querySelector('.setup-similar');
 
 
 // отображение блока .setup и .setup-similar
-WIZARDS_DIALOG.classList.remove('hidden');
+// WIZARDS_DIALOG.classList.remove('hidden');
 SETUP_SIMILAR.classList.remove('hidden');
 
 
@@ -63,7 +71,7 @@ var getRandomElement = function (array) {
 // массив 4х сгенерированных JS объектов (волшебников)
 var randomWizards = function () {
   var arr = [];
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < Wizard.CONST; i++) {
     arr.push({
       firstName: getRandomElement(Wizard.FIRST_NAME),
       lastName: getRandomElement(Wizard.LAST_NAME),
@@ -96,4 +104,69 @@ var renderWizards = function () {
 
 renderWizards();
 
+
+// next lesson
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var SETUP = WIZARDS_DIALOG;
+var SETUP_OPEN = document.querySelector('.setup-open');
+var SETUP_CLOSE = SETUP.querySelector('.setup-close');
+var clickedElement = null;
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    onClosePopup();
+  }
+};
+var onOpenPopup = function (evt) {
+  clickedElement = evt.currentTarget;
+  if (clickedElement) {
+    SETUP.classList.remove('hidden');
+  }
+  document.addEventListener('keydown', onPopupEscPress);
+};
+var onClosePopup = function (evt) {
+  clickedElement = evt.currentTarget;
+  if (clickedElement) {
+    SETUP.classList.add('hidden');
+  }
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+// по клику на элемент - закрытие, открытие Popup
+SETUP_OPEN.addEventListener('click', onOpenPopup);
+SETUP_CLOSE.addEventListener('click', onClosePopup);
+
+// событие Popup по ENTER
+SETUP_OPEN.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    onOpenPopup();
+  }
+});
+SETUP_CLOSE.addEventListener('keydown', function (evt) {
+  evt.stopPropagation();
+  if (evt.keyCode === ENTER_KEYCODE) {
+    onClosePopup();
+  }
+});
+
+var SETUP_WIZARD = SETUP.querySelector('.setup-wizard');
+var WIZARD_COAT = SETUP_WIZARD.querySelector('.wizard-coat');
+var WIZARD_EYES = SETUP_WIZARD.querySelector('.wizard-eyes');
+var SETUP_FIREBALL_WRAP = document.querySelector('.setup-fireball-wrap');
+
+var onChangeCoatColor = function () {
+  WIZARD_COAT.style.fill = getRandomElement(Wizard.COAT_COLOR);
+};
+var onChangeEyesColor = function () {
+  WIZARD_EYES.style.fill = getRandomElement(Wizard.EYES_COLOR);
+};
+var onChangeFireballColor = function () {
+  SETUP_FIREBALL_WRAP.style.background = getRandomElement(Wizard.FIREBALL);
+};
+
+WIZARD_COAT.addEventListener('click', onChangeCoatColor);
+WIZARD_EYES.addEventListener('click', onChangeEyesColor);
+SETUP_FIREBALL_WRAP.addEventListener('click', onChangeFireballColor);
 
