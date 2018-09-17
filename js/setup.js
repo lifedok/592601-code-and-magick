e4d65691dee3777dfef35ff7ae593be71c8b5efd
lifedok <lifedok@gmail.com>
2018-09-18 01:39:23 +0300

    Fix

--- js/setup.js
+++ js/setup.js
@@ -113,13 +113,6 @@ var SETUP = WIZARDS_DIALOG;
 var SETUP_OPEN = document.querySelector('.setup-open');
 var SETUP_CLOSE = SETUP.querySelector('.setup-close');
 
-// не смог понять, как сделать так,
-// чтобы по ENTER на input не закрывалось окно.
-// А закрывалось только по ENTER на крестик. Это актуально, когда не стоит action в form.
-// Из-за того, что сейчас в index.html в form.setup-wizard-form стоит action, то
-// по ENTER отправляет форму.
-// Если правильно понимаю, то только этот кейс до полной готовности остался.
-
 var onPopupEscPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     onClosePopup();
@@ -145,8 +138,6 @@ SETUP_OPEN.addEventListener('keydown', function (evt) {
   }
 });
 SETUP_CLOSE.addEventListener('keydown', function (evt) {
-  console.log('jkgh', !evt.target.classList.contains('setup-user-name'));
-  console.log('jkgh', evt.target.classList.contains('setup-close'));
   if (evt.keyCode === ENTER_KEYCODE && !evt.target.classList.contains('.setup-user-name')) {
     onClosePopup();
   }
